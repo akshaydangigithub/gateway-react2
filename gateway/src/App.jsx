549299849ada4c5822172b1665d611104e6d9c02
Hariom dangi <hdangi224@gmail.com>
2025-05-13 16:37:47 +0530

    components

--- gateway/src/App.jsx
+++ gateway/src/App.jsx
@@ -1,36 +1,20 @@
 import React from "react";
+import Card from "./components/Card.jsx";
+// import { greet, greet2 } from "./New";
 
 const App = () => {
-  let cardData = [
-    {
-      img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
-      name: "Akshay Dangi",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. onsectetur distinctio voluptatibus deserunt.",
-    },
-    {
-      img: "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
-      name: "Naman Dangi",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. onsectetur distinctio voluptatibus deserunt.",
-    },
-    {
-      img: "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
-      name: "Naman Dangi",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. onsectetur distinctio voluptatibus deserunt.",
-    },
-  ];
-
-  console.log(cardData);
+  // greet();
+  // greet2();
 
   return (
-    <main>
-      {cardData.map((data, index) => (
-        <div key={index} className="card">
-          <img src={data.img} alt="" />
-          <h1>{data.name}</h1>
-          <p>{data.desc}</p>
-        </div>
-      ))}
-    </main>
+    <div className="grid grid-cols-4 gap-4">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
   );
 };
 
---
+++ gateway/src/New.js
@@ -0,0 +1,9 @@
+function greet() {
+  console.log("Hello welcome!!");
+}
+
+function greet2() {
+  console.log("Hello again");
+}
+
+export { greet2, greet };
