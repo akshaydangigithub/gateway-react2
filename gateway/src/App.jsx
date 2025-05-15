61bbf1bb7de79651af4cabf435c4560888bd1c88
Hariom dangi <hdangi224@gmail.com>
2025-05-15 16:56:53 +0530

    Form Handling

--- gateway/src/App.jsx
+++ gateway/src/App.jsx
@@ -1,18 +1,39 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import NotFound from "./pages/NotFound";
+import React, { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let newUser = {
+      name,
+      email
+    }
+    console.log(newUser);
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 w-96 bg-red-100 p-5"
+      >
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="email"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
