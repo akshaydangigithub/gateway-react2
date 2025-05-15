3d126d4fcd35c897ee82059f3f649b1e265e458d
Hariom dangi <hdangi224@gmail.com>
2025-05-15 17:51:48 +0530

    user crud

--- gateway/src/App.jsx
+++ gateway/src/App.jsx
@@ -1,38 +1,107 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [users, setUsers] = useState([]);
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [activeIndex, setActivindex] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
-    let newUser = {
+    let newuser = {
       name,
-      email
-    }
-    console.log(newUser);
+      email,
+    };
+    console.log(newuser);
+
+    setUsers([...users, newuser]);
+    setname("");
+    setemail("");
+  }
+
+  function handleDelete(index) {
+    let newUsers = users.filter((user, idx) => {
+      return idx !== index;
+    });
+
+    setUsers(newUsers);
+  }
+
+  function handleEdit(index) {
+    setActivindex(index);
+    let editedUser = users[index];
+    setname(editedUser.name);
+    setemail(editedUser.email);
+  }
+
+  function handleUpdate(e) {
+    e.preventDefault();
+
+    users[activeIndex].name = name
+    users[activeIndex].email = email
+
+    setActivindex(null)
+    setemail("")
+    setname("")
   }
 
   return (
     <div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-4 w-96 bg-red-100 p-5"
-      >
+      <form className="border-2 w-96 p-5 rounded-xl">
         <input
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setname(e.target.value)}
           type="text"
-          placeholder="Name"
-        />
+          placeholder="Name...."
+        />{" "}
+        <br /> <br />
         <input
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setemail(e.target.value)}
           type="email"
-          placeholder="email"
-        />
-        <button type="submit">Submit</button>
+          placeholder="Email...."
+        />{" "}
+        <br /> <br />
+        {activeIndex === null ? (
+          <button onClick={handleSubmit}>Submit</button>
+        ) : (
+          <button onClick={handleUpdate}>Update</button>
+        )}
       </form>
+
+      <table className="mt-5 w-1/2 border-2">
+        <thead>
+          <tr>
+            <th className="border-2 p-3">S.No.</th>
+            <th className="border-2 p-3">Name</th>
+            <th className="border-2 p-3">Email</th>
+            <th className="border-2 p-3">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr>
+              <td className="border-2 p-3">{index + 1}</td>
+              <td className="border-2 p-3">{user.name}</td>
+              <td className="border-2 p-3">{user.email}</td>
+              <td className="border-2 p-3">
+                <span
+                  onClick={() => handleDelete(index)}
+                  className="text-red-500 underline text-sm cursor-pointer"
+                >
+                  Delete
+                </span>
+                <span
+                  onClick={() => handleEdit(index)}
+                  className="text-blue-700 underline text-sm cursor-pointer ml-4"
+                >
+                  Edit
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
